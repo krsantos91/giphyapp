628adb57211230719dc2c0a13beb029a3cd586b0
Kevin Santos <kevinsantos@Kevins-MBP.charter.com>
2017-07-23 07:19:23 -0700

    lazy load and block invalid input

--- assets/app.js
+++ assets/app.js
@@ -3,12 +3,13 @@ var giphySearch = {
 
 	temp: "",
 
+	contentIndex: 1,
+
 	newSearch: function(event){
-		event.preventDefault();
-		$("#search-input").empty()
-		console.log("is this working");
+		$("#loadmore").empty();
+		giphySearch.contentIndex = 1;
     	var newGif = $("#search-input").val().trim();
-    	console.log(newGif);
+    	$("#search-input").val("")
     	if (giphySearch.index.indexOf(newGif) == -1){
        	 	giphySearch.index.push(newGif);
        	 	giphySearch.renderButtons();
@@ -18,21 +19,34 @@ var giphySearch = {
 			xhr.done(function(response) { 
 				$("#giphy-info").empty();
 				var results = response.data;
-				results.forEach(function(picture){
-					var container = $("<div class='col-sm-4 animated zoomIn btn' style='margin-bottom:5px'>");
-					var gifDiv = $("<div class='gif-container container'>");
-					var rating = $("<span style='margin-right:5px'>").text("Rating: " + picture.rating.toUpperCase());
-					gifDiv.attr("data-state","still")
-					gifDiv.attr("data-still-url",picture.images.fixed_height_still.url);
-					gifDiv.attr("data-animate-url",picture.images.fixed_height.url);
-					gifDiv.css("background-image", "url('" + picture.images.fixed_height_still.url + "')")
-					var bookmarkButton = $("<span class='btn btn-success bookmark'>");
-					bookmarkButton.html('<i class="fa fa-bookmark" aria-hidden="true"></i></i><span> Bookmark</span>');
-					bookmarkButton.attr("data-bookmark-url",picture.images.downsized.url);
-					bookmarkButton.attr("data-url",picture.images.fixed_height.url);
-					container.append(gifDiv).append(rating).append(bookmarkButton);
-					$("#giphy-info").append(container);
-				});
+				if (response.pagination.total_count === 0){
+					var message = $("<div class='container-fluid'>");
+					message.html("<h3>Your search returned 0 results, please try something else.</h3>")
+					delete giphySearch.index[giphySearch.index.length-1];
+					giphySearch.renderButtons();
+					$("#giphy-info").append(message);
+				}
+				else{
+					results.forEach(function(picture){
+						var container = $("<div class='col-sm-4 animated zoomIn' style='margin-bottom:5px'>");
+						var gifDiv = $("<div class='gif-container container btn'>");
+						var rating = $("<div>").text("Rating: " + picture.rating.toUpperCase());
+						gifDiv.attr("data-state","still")
+						gifDiv.attr("data-still-url",picture.images.fixed_height_still.url);
+						gifDiv.attr("data-animate-url",picture.images.fixed_height.url);
+						gifDiv.css("background-image", "url('" + picture.images.fixed_height_still.url + "')")
+						var bookmarkButton = $("<span class='btn btn-success bookmark'>");
+						bookmarkButton.html('<i class="fa fa-bookmark" aria-hidden="true"></i></i><span> Bookmark</span>');
+						bookmarkButton.attr("data-bookmark-url",picture.images.downsized.url);
+						bookmarkButton.attr("data-url",picture.images.fixed_height.url);
+						container.append(gifDiv).append(rating).append(bookmarkButton);
+						$("#giphy-info").append(container);
+					});
+					var nextButton = $("<div class='btn btn-warning' id='nextbutton'>");
+					nextButton.attr("data-info",newGif);
+					nextButton.text("Load more");
+					$("#loadmore").append(nextButton);	
+				}		
 			});
 		};
 	},
@@ -50,17 +64,18 @@ var giphySearch = {
 	},
 
 	api: function(event){
+		$("#loadmore").empty();
 		$("#giphy-info").empty();
+		giphySearch.contentIndex = 1;
 		var searchTerm = $(event.currentTarget).attr("data-name");
 		queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm +"&api_key=8925750c46774225a89d94feafb4b3b6&limit=25"
 		var xhr = $.get(queryURL);
 		xhr.done(function(response) { 
-			var results = response.data;
-			var counter = 1;
-			results.forEach(function(picture){
+			var results = response.data;			
+			results.forEach(function(picture){				
 				var container = $("<div class='col-sm-4 animated zoomIn' style='margin-bottom:5px'>");
-				var gifDiv = $("<div class='gif-container container'>");
-				var rating = $("<span style='margin-right:5px'>").text("Rating: " + picture.rating);
+				var gifDiv = $("<div class='gif-container container btn'>");
+				var rating = $("<div>").text("Rating: " + picture.rating.toUpperCase());
 				gifDiv.attr("data-state","still")
 				gifDiv.attr("data-still-url",picture.images.fixed_height_still.url);
 				gifDiv.attr("data-animate-url",picture.images.fixed_height.url);
@@ -71,8 +86,12 @@ var giphySearch = {
 				bookmarkButton.attr("data-url",picture.images.fixed_height.url);
 				container.append(gifDiv).append(rating).append(bookmarkButton)
 				$("#giphy-info").append(container);
-
 			});
+			var nextButton = $("<div class='btn btn-warning' id='nextbutton'>");
+			nextButton.attr("data-info", searchTerm);
+			nextButton.text("Load more");
+			$("#loadmore").append(nextButton);
+
         });
 	},
 
@@ -82,7 +101,7 @@ var giphySearch = {
 		var container = $("<div class='col-xs-4 col-sm-2 col-md-6 animated fadeInUp'>")	;
 		var link = $("<a>");	
 		link.attr("href", $(event.currentTarget).attr("data-url"));
-		link.attr("target","blank");
+		link.attr("target","_blank");
 		var imageDiv = $("<div class='container'>");
 		imageDiv.addClass("savedImages");
 		imageDiv.css('background-image', 'url("'+ imageURL + '")');
@@ -114,7 +133,7 @@ var giphySearch = {
 			$(event.currentTarget).css("background-image", "url('" + animate + "')");
 			$(event.currentTarget).attr("data-state","animated" );
 			$(event.currentTarget).animate({
-				height: '400px',
+				height: '350px',
 				width: '400px'
 			});
 
@@ -130,6 +149,36 @@ var giphySearch = {
 			});			
 		}
 	},
+
+	addContent: function(searchterm){
+		giphySearch.contentIndex++;
+		$("#loadmore").empty();
+		queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchterm +"&api_key=8925750c46774225a89d94feafb4b3b6&limit=25&offset=" + (25*giphySearch.contentIndex);
+		var xhr = $.get(queryURL);
+		xhr.done(function(response) { 
+			var results = response.data;
+			results.forEach(function(picture){
+				var container = $("<div class='col-sm-4 animated zoomIn' style='margin-bottom:5px'>");
+				var gifDiv = $("<div class='gif-container container btn'>");
+				var rating = $("<div>").text("Rating: " + picture.rating.toUpperCase());
+				gifDiv.attr("data-state","still")
+				gifDiv.attr("data-still-url",picture.images.fixed_height_still.url);
+				gifDiv.attr("data-animate-url",picture.images.fixed_height.url);
+				gifDiv.css("background-image", "url('" + gifDiv.attr("data-still-url") + "')");
+				var bookmarkButton = $("<div class='btn btn-success bookmark'>");
+				bookmarkButton.html('<i class="fa fa-bookmark" aria-hidden="true"></i><span> Bookmark</span>');
+				bookmarkButton.attr("data-bookmark-url",picture.images.downsized.url);
+				bookmarkButton.attr("data-url",picture.images.fixed_height.url);
+				container.append(gifDiv).append(rating).append(bookmarkButton)
+				$("#giphy-info").append(container);
+			});
+			var nextButton = $("<div class='btn btn-warning' id='nextbutton'>");
+			nextButton.attr("data-info", searchterm);
+			nextButton.text("Load more");
+			$("#loadmore").append(nextButton);		
+		});
+	}
+
 };
 
 
@@ -148,3 +197,9 @@ $(document).on("click",".bookmark",function(e){
 $(document).on("click",".gif-container",function(e){
 	giphySearch.animate(e);
 })
+
+$(document).on("click","#nextbutton",function(e){
+	giphySearch.addContent($(this).attr("data-info"));
+})
+
+
